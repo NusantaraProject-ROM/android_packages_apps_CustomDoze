0129acc78a64b4706e704b9fe60e0c8a83d9b6f6
Alex <du.alexcruz@gmail.com>
2019-01-29 15:29:36 -0600

    Welcome Custom Doze

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_USE_AAPT2 := true
 
-LOCAL_PACKAGE_NAME := OneplusDoze
+LOCAL_PACKAGE_NAME := CustomDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRIVATE_PLATFORM_APIS := true
