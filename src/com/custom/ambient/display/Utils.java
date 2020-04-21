4115ad6f078966366dd6fd61acc8a2d578ae5354
Anushek Prasal <anushekprasal@gmail.com>
2020-04-21 15:16:42 +0530

    CustomDoze: Don't pulse if Screen Off FOD is enabled

--- src/com/custom/ambient/display/Utils.java
+++ src/com/custom/ambient/display/Utils.java
@@ -166,9 +166,13 @@ public final class Utils {
     }
 
     protected static void launchDozePulse(Context context) {
-        if (DEBUG) Log.d(TAG, "Launch doze pulse");
-        context.sendBroadcastAsUser(new Intent(DOZE_INTENT),
-                new UserHandle(UserHandle.USER_CURRENT));
+        final boolean fodEnabled = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.SCREEN_OFF_FOD, 0) != 0;
+        if (!fodEnabled) {
+            if (DEBUG) Log.d(TAG, "Launch doze pulse");
+            context.sendBroadcastAsUser(new Intent(DOZE_INTENT),
+                    new UserHandle(UserHandle.USER_CURRENT));
+        }
     }
 
     protected static boolean sensorsEnabled(Context context) {
